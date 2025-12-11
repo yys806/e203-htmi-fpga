7ebb77bbb46a15d878f6267b107d48dfa0cbe018
yys806 <3492675568@qq.com>
2025-12-11 21:31:50 +0800

    修改软件代码4

--- sim/iverilog-lnx/sys_tb_top.sv
+++ sim/iverilog-lnx/sys_tb_top.sv
@@ -1,7 +1,7 @@
 `timescale 1ns/10ps
 `define USING_IVERILOG
 
-module sys_tb_top();
+module sys_tb_top;
 
   reg  clk;
   reg  lfextclk;
@@ -9,7 +9,7 @@ module sys_tb_top();
 
   wire hfclk = clk;
 
-  // GPIO ?? SoC?bit16 ????? UART RX
+  // GPIO into SoC, bit16 as terminal UART RX
   reg  [31:0] gpio_in_tb;
   wire [31:0] gpio_out_tb;
   wire term_uart_rx;
@@ -18,13 +18,7 @@ module sys_tb_top();
 `ifdef USING_IVERILOG
   initial begin
     $dumpfile("waveout.vcd");
-    // ?? VCD ?????????
     $dumpvars(0, sys_tb_top);
-    $dumpoff;
-    #100_000;    // 0.1ms ????
-    $dumpon;
-    #5_900_000;  // ???? 6.0ms
-    $dumpoff;
   end
 `endif
 
@@ -35,7 +29,7 @@ module sys_tb_top();
   end
 `endif
 
-  // ???????? 8ms????????????
+  // total sim time
   initial begin
     #20ms;
     $display("[TB] finished at %0t", $time);
@@ -43,37 +37,32 @@ module sys_tb_top();
   end
 
   initial begin
-    clk        <= 0;
-    lfextclk   <= 0;
-    rst_n      <= 0;
-    gpio_in_tb <= 32'hFFFF_FFFF; // ???
-    #320us rst_n <= 1;
+    clk        = 0;
+    lfextclk   = 0;
+    rst_n      = 0;
+    gpio_in_tb = 32'hFFFF_FFFF; // idle high
+    #320us rst_n = 1;
   end
 
-  always begin
-     #18.52 clk <= ~clk;
-  end
+  always #18.52 clk = ~clk;       // ~27MHz
+  always #33    lfextclk = ~lfextclk; // slow clock
 
-  always begin
-     #33 lfextclk <= ~lfextclk;
-  end
-
-  // UART ??? SoC?115200bps?bit ??? 8.68us?
+  // UART send to SoC (115200bps, bit time ~8.68us)
   localparam int UART_BIT = 8680; // ns
   task send_uart_byte(input [7:0] b);
     integer i;
     begin
-      gpio_in_tb[16] <= 1'b0; #UART_BIT;        // start
+      gpio_in_tb[16] = 1'b0; #UART_BIT;        // start
       for(i=0; i<8; i=i+1) begin
-        gpio_in_tb[16] <= b[i]; #UART_BIT;      // data
+        gpio_in_tb[16] = b[i]; #UART_BIT;      // data
       end
-      gpio_in_tb[16] <= 1'b1; #UART_BIT;        // stop
+      gpio_in_tb[16] = 1'b1; #UART_BIT;        // stop
     end
   endtask
 
-  // ?????????
+  // send sample data
   initial begin
-    #(400_000); // ??????400us
+    #(400_000); // wait ~400us after reset
     send_uart_byte("A");
     send_uart_byte("B");
     send_uart_byte("C");
@@ -82,34 +71,28 @@ module sys_tb_top();
   end
 
   e203_soc_demo uut (
-        .clk_in              (clk),  
-
-        .tck                 (), 
-        .tms                 (), 
-        .tdi                 (), 
-        .tdo                 (),  
-
+        .clk_in              (clk),
+        .tck                 (),
+        .tms                 (),
+        .tdi                 (),
+        .tdo                 (),
         .gpio_in             (gpio_in_tb),
         .gpio_out            (gpio_out_tb),
         .qspi_in             (),
-        .qspi_out            (),      
-        .qspi_sck            (),  
-        .qspi_cs             (),   
-
-        .erstn               (rst_n), 
-
-        .dbgmode0_n          (1'b1), 
+        .qspi_out            (),
+        .qspi_sck            (),
+        .qspi_cs             (),
+        .erstn               (rst_n),
+        .dbgmode0_n          (1'b1),
         .dbgmode1_n          (1'b1),
         .dbgmode3_n          (1'b1),
-  
-        .bootrom_n           (1'b0), 
-  
-        .aon_pmu_dwakeup_n   (), 
-        .aon_pmu_padrst      (),    
-        .aon_pmu_vddpaden    () 
+        .bootrom_n           (1'b0),
+        .aon_pmu_dwakeup_n   (),
+        .aon_pmu_padrst      (),
+        .aon_pmu_vddpaden    ()
     );
 
-  // ??????? PLL ?????????? lock ??????????
+  // force PLL lock and clocks for sim
   initial begin
     force uut.clk_unit.lock = 1'b1;
     force uut.clk_unit.clkout_system = clk;
